faf1302bd550ad7920a55d1920fbc28166fe7039
NoBypass <nobypass.yt@gmail.com>
2024-03-07 20:58:51 +0100

    refactor tests

--- actions_test.go
+++ actions_test.go
@@ -82,72 +82,86 @@ func TestCreate(t *testing.T) {
 
 func TestSelect(t *testing.T) {
 	t.Run("With a minimal select query", func(t *testing.T) {
-		_, err := testDB.Select()
+		test := SampleModel{}
+		err := testDB.Select(&test)
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With an id", func(t *testing.T) {
-		_, err := testDB.Select(ID("123"))
+		test := SampleModel{}
+		err := testDB.Select(&test, ID("123"))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel:123;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With a ranged id", func(t *testing.T) {
-		_, err := testDB.Select(ID([2]any{SampleID{123}, SampleID{456}}))
+		test := SampleModel{}
+		err := testDB.Select(&test, ID([2]any{SampleID{123}, SampleID{456}}))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel:[123]..[456];", testDB.db.(*DBMock).query)
 	})
 	t.Run("With fields", func(t *testing.T) {
-		_, err := testDB.Select(Fields("id", "name"))
+		test := SampleModel{}
+		err := testDB.Select(&test, Fields("id", "name"))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT id, name FROM SampleModel;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With omit", func(t *testing.T) {
-		_, err := testDB.Select(Omit("id", "name"))
+		test := SampleModel{}
+		err := testDB.Select(&test, Omit("id", "name"))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * OMIT id, name FROM SampleModel;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With only", func(t *testing.T) {
-		_, err := testDB.Select(Only())
+		test := SampleModel{}
+		err := testDB.Select(&test, Only())
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM ONLY SampleModel;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With where", func(t *testing.T) {
-		_, err := testDB.Select(Where(`name = "foo"`))
+		test := SampleModel{}
+		err := testDB.Select(&test, Where(`name = "foo"`))
 		assert.NoError(t, err)
 		assert.Equal(t, `SELECT * FROM SampleModel WHERE name = "foo";`, testDB.db.(*DBMock).query)
 	})
 	t.Run("With group by", func(t *testing.T) {
-		_, err := testDB.Select(GroupBy("name", "age"))
+		test := SampleModel{}
+		err := testDB.Select(&test, GroupBy("name", "age"))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel GROUP BY name, age;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With order by", func(t *testing.T) {
-		_, err := testDB.Select(OrderBy(Asc("name"), Desc("age")))
+		test := SampleModel{}
+		err := testDB.Select(&test, OrderBy(Asc("name"), Desc("age")))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel ORDER BY name ASC, age DESC;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With limit", func(t *testing.T) {
-		_, err := testDB.Select(Limit(10))
+		test := SampleModel{}
+		err := testDB.Select(&test, Limit(10))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel LIMIT 10;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With start", func(t *testing.T) {
-		_, err := testDB.Select(Start(10))
+		test := SampleModel{}
+		err := testDB.Select(&test, Start(10))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel START 10;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With fetch", func(t *testing.T) {
-		_, err := testDB.Select(Fetch("group.sub"))
+		test := SampleModel{}
+		err := testDB.Select(&test, Fetch("group.sub"))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel FETCH group.sub;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With timeout", func(t *testing.T) {
-		_, err := testDB.Select(Timeout(10 * time.Second))
+		test := SampleModel{}
+		err := testDB.Select(&test, Timeout(10*time.Second))
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel TIMEOUT 10000ms;", testDB.db.(*DBMock).query)
 	})
 	t.Run("With parallel", func(t *testing.T) {
-		_, err := testDB.Select(Parallel())
+		test := SampleModel{}
+		err := testDB.Select(&test, Parallel())
 		assert.NoError(t, err)
 		assert.Equal(t, "SELECT * FROM SampleModel PARALLEL;", testDB.db.(*DBMock).query)
 	})
